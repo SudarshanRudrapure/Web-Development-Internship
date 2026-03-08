503bc1f9210df5aa387fab33b0b27e1b81dee82b
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-03-08 23:55:50 +0530

    Javascript Datatypes

---
+++ week-5/Day 31/datatypes.js
@@ -0,0 +1,61 @@
+
+var x = "Hello";
+alert(typeof x); // string
+
+
+var x = "JavaScript";
+alert(typeof x); // string
+
+var x = 10;
+alert(typeof x); // number
+
+var x = 25.5;
+alert(typeof x); // number
+
+var x = true;
+alert(typeof x); // boolean
+
+var x = false;
+alert(typeof x); // boolean
+
+var x;
+alert(typeof x); // undefined
+
+var x = null;
+alert(typeof x); // object
+
+var name = "Alex";
+alert(typeof name); // string
+
+var age = 21;
+alert(typeof age); // number
+
+var price = 99.99;
+alert(typeof price); // number
+
+var isLogin = true;
+alert(typeof isLogin); // boolean
+
+var city = "Bangalore";
+alert(typeof city); // string
+
+var marks = 85;
+alert(typeof marks); // number
+
+var temperature = 36.7;
+alert(typeof temperature); // number
+
+var isOnline = false;
+alert(typeof isOnline); // boolean
+
+var course = "Web Development";
+alert(typeof course); // string
+
+var count = 100;
+alert(typeof count); // number
+
+var status = true;
+alert(typeof status); // boolean
+
+var data;
+alert(typeof data); // undefined
