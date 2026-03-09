9208c8f05c564d6feb54b0a0bafb917373fe114c
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-03-09 22:42:58 +0530

    JavaScript Variablrs

---
+++ week-6/Day 32/varexample.js
@@ -0,0 +1,49 @@
+// 1 Declaring a variable
+var x;
+x = 5;
+alert(x);
+
+// 2 Declaring and assigning value
+var word = "John";
+alert(word);
+
+// 3 Number variable
+var age = 21;
+alert(age);
+
+// 4 Using variable in expression
+var a = 10;
+var b = 5;
+var sum = a + b;
+alert(sum);
+
+// 5 Subtraction using variables
+var x = 20;
+var y = 8;
+var result = x - y;
+alert(result);
+
+// 6 Multiplication
+var a = 4;
+var b = 3;
+var result = a * b;
+alert(result);
+
+// 7 Division
+var a = 20;
+var b = 4;
+var result = a / b;
+alert(result);
+
+// 8 Changing variable value
+var score = 50;
+score = 80;
+alert(score);
+
+// 9 String variable
+var city = "Bangalore";
+alert(city);
+
+// 10 Boolean variable
+var isStudent = true;
+alert(isStudent);
