fe53b3ccc46fc2bbec016099590b76205eb5aca8
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-03-10 22:29:55 +0530

    JavaScript-Basics

---
+++ week-6/Day 33/simple.js
@@ -0,0 +1,11 @@
+var names = "John";
+console.log(names);
+
+
+// var msg = "Welcome!";
+// alert(msg);
+
+// confirm("Do you want to continue?");
+
+// var name = prompt("Enter your name");
+// alert(name);
