6fdd8977ca757b12f5bd560424c53b5fee4a9ddd
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-03-15 23:52:17 +0530

    JavaScript Operators

---
+++ week-6/Day 35/Comparison.js
@@ -0,0 +1,18 @@
+// Comparison Operators
+// Examples
+var x = 100;
+var y = 85;
+
+console.log(x == y); // outputs FALSE
+var x = 85;
+var y = "85";
+
+console.log(x === y); // outputs FALSE
+var x = 100;
+var y = 85;
+
+console.log(x != y); // outputs TRUE
+var x = 100;
+var y = 85;
+
+console.log(y <= x); // outputs TRUE
