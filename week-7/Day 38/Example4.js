495329d1807716d889bf5b84b3f01e813017992b
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-03-16 00:23:03 +0530

    Conditional-Statement Based Examples

---
+++ week-7/Day 38/Example4.js
@@ -0,0 +1,12 @@
+var username = "admin";
+var password = "1234";
+
+if (username === "admin" && password === "1234") {
+    console.log("Login Successful");
+}
+else if (username !== "admin") {
+    console.log("Invalid Username");
+}
+else {
+    console.log("Invalid Password");
+}
---
+++ week-7/Day 38/Examples3.js
@@ -0,0 +1,10 @@
+//Even or Odd
+
+var num = 17;
+
+if (num % 2 == 0) {
+    console.log("The number is Even");
+}
+else {
+    console.log("The number is Odd");
+}
