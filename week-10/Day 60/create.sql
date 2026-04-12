ea8699b586cb57f52f6522836fb54d28d7d62b4a
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-04-12 12:45:42 +0530

    sql basics

---
+++ week-10/Day 60/create.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE testdb;
+
+USE testdb;
+
+/*If categories table exists, delete it*/
+DROP TABLE IF EXISTS `categories`;
+
+/*Create categories table*/
+CREATE TABLE `categories` (
+ `categoryCode` int(10) NOT NULL,
+ `categoryName` varchar(50) NOT NULL,
+ `categoryDescription` varchar(120) NOT NULL,
+ PRIMARY KEY (`categoryCode`)
+);
+
+/*If products table exists, delete it*/
+DROP TABLE IF EXISTS `products`;
+
+/*Create products table*/
+CREATE TABLE `products`(
+ `productCode` int(11) NOT NULL,
+ `productName` varchar(50) NOT NULL,
+ `productPrice` int(8) NOT NULL,
+ `categoryCode` int(10) NOT NULL,
+ PRIMARY KEY (`productCode`),
+ KEY `categoryCode` (`categoryCode`),
+ CONSTRAINT `products_fk` FOREIGN KEY (`categoryCode`) REFERENCES `categories` (`categoryCode`)
+);
+
+/*Alter the products table and add productDescription column*/
+ALTER TABLE products
+ADD productDescription varchar(120) NOT NULL; 
