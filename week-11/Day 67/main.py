fc96646b7310acca6a111b1854f68e86bafb75ef
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-04-22 13:22:32 +0530

    python-conditions and functions

---
+++ week-11/Day 67/main.py
@@ -0,0 +1,7 @@
+weather_conditions = "fair"
+if weather_conditions =="sunny":
+ print("Don't forget your sunglasses")
+elif weather_conditions =="rainy":
+ print("Be sure to grab your umbrella")
+else:
+ print("You're all good to go!")
