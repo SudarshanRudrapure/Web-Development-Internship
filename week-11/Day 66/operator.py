d887c62177cd5c96c740556f99af70b34298dfba
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-04-22 13:01:55 +0530

    python-variables and operators

---
+++ week-11/Day 66/operator.py
@@ -0,0 +1,25 @@
+#Arithmetic
+a = 2+8
+print(a)
+
+b=2*(6+4)
+print(b)
+
+c=10%3
+print(c)
+
+
+#Comparison
+print(15==12)
+print(15==15)
+print(15=="15")
+print(15!=10)
+print(15!="15")
+print(20<10)
+print(10<10)
+print(10<=10)
+
+
+#Logical
+print(10<20 and 5>2)
+print(10>20 or 5>2)
