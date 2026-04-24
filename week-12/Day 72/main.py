901d281eedde3ad1460b57ad9ef045a9d4ad3d7e
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-04-24 10:33:20 +0530

    simple example

--- week-12/Day 72/main.py
+++ week-12/Day 72/main.py
@@ -1,7 +1,9 @@
 import sqlite3
+
 #CREATE DATABASE AND CONNECT
 conn = sqlite3.connect('employee_database.db')
 print ("Database has been opened");
+
 #CREATE EMPLOYEES TABLE
 conn.execute('''CREATE TABLE employees
  (EMP_ID INT PRIMARY KEY NOT NULL,
@@ -9,6 +11,8 @@ conn.execute('''CREATE TABLE employees
  DEPARTMENT_NAME CHAR(40)NOT NULL,
  REGION CHAR(50),
  SALARY REAL);''')
+
 print ("Table created successfully");
+
 #CLOSE DATABASE AFTER CREATE OPERATION HAS BEEN EXECUTED
 conn.close()
