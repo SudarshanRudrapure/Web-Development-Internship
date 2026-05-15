6e9736e1f1d4f345e3b92dc4d190a90f2104a214
Sudarshan Rudrapure <sudarshanrudrapure@gmail.com>
2026-05-15 18:25:02 +0530

    simple examples

--- week-5/Assignment/assignment/sum_numbers.py
+++ week-5/Assignment/assignment/sum_numbers.py
@@ -1,6 +1,6 @@
+
 def sum_numbers(N):
     total = 0
-
     for i in range(1, N + 1):
         if i > 50:
             print("Loop stopped early at:", i)
@@ -12,5 +12,4 @@ def sum_numbers(N):
 
 num = int(input("Enter a number: "))
 result = sum_numbers(num)
-
 print("Sum of numbers from 1 to", num, "is:", result)
