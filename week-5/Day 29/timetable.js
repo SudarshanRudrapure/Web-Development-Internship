65105bcdbee8c0d9a31354efe5dd1e92b5204b9f
Sudarshan Rudrapure <sudarshanrudrapure@gmail.com>
2026-05-15 18:21:32 +0530

    simple JS project

--- week-5/Day 29/timetable.js
+++ week-5/Day 29/timetable.js
@@ -4,11 +4,13 @@
  var container = document.querySelector("#container");
  var body = document.querySelector("body");
 
+ 
  var monday=["ECO323","IFS353","MAN305","IFS362"];
  var tuesday=["IFS353","MAN305"];
  var wednesday=["IFS362","MAN303","ECO341"];
  var thursday=["IFS353","MAN302","STATS302"];
  var friday=["No class! YAY! :)"];
+
 
  //when hover over button change colour
  function setHoverColor() {
@@ -19,6 +21,7 @@
  btn.style.background = "";
  }
  // Assigning event listeners to the button
+
 
 btn.addEventListener("mouseover", setHoverColor);
 btn.addEventListener("mouseout", setNormalColor);
