ce8bd278130f32f56f09bd8c0fe6419baad7c89c
Sudarshan Rudrapure <sudarshanrudrapure@gmail.com>
2026-05-20 17:32:02 +0530

    python-loops and modules

--- week-12/Day 68/loops.py
+++ week-12/Day 68/loops.py
@@ -5,11 +5,13 @@ for i in range(10):
 i = 0
 for i in range(10):
  print("This is loop " + str(i))
+
 # while loop
 i = 0
 while i <= 10:
  print("This is loop " + str(i))
  i = i + 1
+ 
 # decrement while loop
 i = 10
 while i >= 0:
--- week-12/Day 68/shape.py
+++ week-12/Day 68/shape.py
@@ -1,3 +1,4 @@
+## Example 
 import area_of_shapes as shapes
 shapes.calculate_area_rect(20,10)
 shapes.calculate_area_square(5)
