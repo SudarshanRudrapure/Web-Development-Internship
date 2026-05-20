cd35d217e1f4ec819ff477109e920248d3bef02a
Sudarshan Rudrapure <sudarshanrudrapure@gmail.com>
2026-05-20 17:22:47 +0530

    updated

--- week-12/Day 68/area_of_shapes.py
+++ week-12/Day 68/area_of_shapes.py
@@ -1,8 +1,10 @@
 ##calculating the area of a rectangle function
 def calculate_area_rect(length,width):
  area = length*width
+
  print(area)
-##calculating the area of a square function
+##calculating the area of a square function 
+
 def calculate_area_square(a):
  area = a*a
  print(area)
