ba89a2807187286d7d712025f8651365ff3de0f7
Sudarshan Rudrapure <sudarshanrudrapure@gmail.com>
2026-05-21 22:08:22 +0530

    about APIs

--- week-13/Day 77/APIs/get_data.py
+++ week-13/Day 77/APIs/get_data.py
@@ -1,3 +1,4 @@
+## Example
 import requests
 get_requests = requests.get('http://127.0.0.1:8000/random')
 print(get_requests.json())
--- week-13/Day 77/examples-APIs/simple.py
+++ week-13/Day 77/examples-APIs/simple.py
@@ -2,11 +2,9 @@ from fastapi import FastAPI
 import random
 app = FastAPI()
 data = {'fname': 'John', 'lname': 'Doe', 'age': 25, 'job_title': 'engineer', 'emp_id': 5}
-
 @app.get('/')
 async def root():
  return data
- 
 @app.get('/random')
 async def rand_emp_id():
  rand:int = random.randint(0,50)
