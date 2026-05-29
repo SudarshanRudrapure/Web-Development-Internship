734e591fc9bf13ec5e68c770d0b5947a6fad1004
Sudarshan Rudrapure <sudarshanrudrapure@gmail.com>
2026-05-29 13:17:25 +0530

    React-Hook-useEffect

--- week-15/Day 89/UseEffectExample.js
+++ week-15/Day 89/UseEffectExample.js
@@ -5,23 +5,17 @@ function UseEffectExample(){
     const [count, setCount] = useState(0);
 
     useEffect(() => {
-
         console.log("Component Rendered");
-
     }, [count]);
 
     return(
 
         <div>
-
             <h1>useEffect Example</h1>
-
             <h2>Count : {count}</h2>
-
             <button onClick={() => setCount(count + 1)}>
                 Increase
             </button>
-
         </div>
 
     );
