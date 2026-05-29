98128996963300111ef8f6d96f3a4999645d767d
Sudarshan Rudrapure <sudarshanrudrapure@gmail.com>
2026-05-29 12:32:09 +0530

    Form Example

--- week-15/Day 85/FormExample.js
+++ week-15/Day 85/FormExample.js
@@ -3,15 +3,11 @@ function App(){
     return(
 
         <form>
-
             <input type="text" placeholder="Enter Name" />
-
             <button type="submit">
                 Submit
             </button>
-
         </form>
-
     );
 
 }
