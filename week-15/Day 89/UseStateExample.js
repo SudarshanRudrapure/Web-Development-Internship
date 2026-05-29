7c021495e94301effacc55d24bccdaada05abcff
Sudarshan Rudrapure <sudarshanrudrapure@gmail.com>
2026-05-29 13:19:49 +0530

    React-Hook-useState

--- week-15/Day 89/UseStateExample.js
+++ week-15/Day 89/UseStateExample.js
@@ -11,27 +11,19 @@ function UseStateExample(){
     }
 
     function decreaseCount(){
-
         setCount(count - 1);
-
     }
 
     return(
-
         <div>
-
             <h1>useState Example</h1>
-
             <h2>Count : {count}</h2>
-
             <button onClick={increaseCount}>
                 Increase
             </button>
-
             <button onClick={decreaseCount}>
                 Decrease
             </button>
-
         </div>
 
     );
