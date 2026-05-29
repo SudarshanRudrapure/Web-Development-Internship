5acf14133b6782b80ebd496ec117673ffcd32509
Sudarshan Rudrapure <sudarshanrudrapure@gmail.com>
2026-05-29 13:19:09 +0530

    React-Hook-useRef

--- week-15/Day 89/UseRefExample.js
+++ week-15/Day 89/UseRefExample.js
@@ -13,19 +13,15 @@ function UseRefExample(){
     return(
 
         <div>
-
             <h1>useRef Example</h1>
-
             <input
                 type="text"
                 ref={inputRef}
                 placeholder="Enter Name"
             />
-
             <button onClick={focusInput}>
                 Focus Input
             </button>
-
         </div>
 
     );
