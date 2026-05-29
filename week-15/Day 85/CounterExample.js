08ca4aa211b344f50779bd28ff289b2288825d46
Sudarshan Rudrapure <sudarshanrudrapure@gmail.com>
2026-05-29 12:31:21 +0530

    simple counter example

--- week-15/Day 85/CounterExample.js
+++ week-15/Day 85/CounterExample.js
@@ -3,29 +3,22 @@ import { useState } from "react";
 function App(){
 
     const [count, setCount] = useState(0);
-
     function increase(){
-
         setCount(count + 1);
 
     }
 
     return(
-
         <div>
 
             <h1>{count}</h1>
-
             <button onClick={increase}>
                 Increase
             </button>
 
         </div>
-
     );
-
 }
-
 export default App;
 
 /*
